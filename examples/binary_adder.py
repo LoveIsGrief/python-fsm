46218a8de4e2ce4d1eca11d3af282fb57bcff5fe
Michael J. Vogel <michael@wildmoka.com>
2016-12-01 15:43:35 +0100

    Add examples

---
+++ examples/binary_adder.py
@@ -0,0 +1,23 @@
+from fsm import MealyMachine, State
+
+adder = MealyMachine('Binary addition')
+
+carry = State('carry')
+nocarry = State('no carry', initial=True)
+
+nocarry[(1, 0), 1] = nocarry
+nocarry[(0, 1), 1] = nocarry
+nocarry[(0, 0), 0] = nocarry
+nocarry[(1, 1), 0] = carry
+
+carry[(1, 1), 1] = carry
+carry[(0, 1), 0] = carry
+carry[(1, 0), 0] = carry
+carry[(0, 0), 1] = nocarry
+
+number1 = list(int (i) for i in '0001010')
+number2 = list(int (i) for i in '0001111')
+
+inputs = zip(number1, number2)
+
+print list(adder.process(inputs[::-1]))[::-1]
